f24bcd2656102f13ef1341f94e6b798146f76ccb
devHikary <wrkkarina@gmail.com>
2021-10-16 17:59:41 -0300

    CSS Modules

--- alura-studies/src/components/Botao/index.tsx
+++ alura-studies/src/components/Botao/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
+import style from './Botao.module.scss';
 
 class Botao extends React.Component{
   render(){
     return(
-      <button>
+      <button className={style.botao}>
         Botão
       </button>
     )
