fb0c9fd73a4894dd1d966d868ff3dbea3507f80b
devHikary <wrkkarina@gmail.com>
2021-10-18 07:21:58 -0300

    exibindo tarefa selecionada

--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -7,10 +7,14 @@ import style from "./App.module.scss";
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
-  const [selecionado, setSelecionado] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
 
-  function selecionaTarefa(tarefaSelecionada, ITarefa): Props{
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
     setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id? true: false
+    })));
   }
 
 
