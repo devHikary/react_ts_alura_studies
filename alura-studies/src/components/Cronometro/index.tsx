6df276b1e0812e78f85605c0edb6d196b2bfe1e4
devHikary <wrkkarina@gmail.com>
2021-10-19 05:58:48 -0300

    Exibindo a hora no relogio

--- alura-studies/src/components/Cronometro/index.tsx
+++ alura-studies/src/components/Cronometro/index.tsx
@@ -1,15 +1,26 @@
-import { tempoParaSegundos } from '../../common/utils/date';
-import Botao from '../Botao';
-import style from './Cronometro.module.scss'
-import Relogio from './Relogio';
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import Botao from "../Botao";
+import style from "./Cronometro.module.scss";
+import Relogio from "./Relogio";
 
-export default function Cronometro() {
-  console.log('conversão: ', tempoParaSegundos('01:01:01'))
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo={tempo} />
       </div>
       <Botao>Começar!</Botao>
     </div>
